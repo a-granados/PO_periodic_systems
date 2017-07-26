c7927d16337ea1c994ab38df153d3cea418fbce7
a-granados <algr@dtu.dk>
2017-07-26 20:30:20 +0200

    New version

--- main.c
+++ main.c
@@ -15,21 +15,22 @@ using std::cout;
 using std::endl;
 using namespace std;
 
+
+
 //Global variables:
 double twopi=8*atan(1.);
 int n=2; //Dimension of the system
 double T=twopi;//Period of the non-autonomous system
 double t0=0;
 
-
-
 void vfield(double t, double *x, int ndim, double *dx);//This function contains the differential equation + the variational equations
 int Newton_method (int q,double *xout,double **DPout);
-int strobomap(double* xini, double tf, double **DP);//This computes the time-qT return time
+int strobomap(double* xini, double tf, double **DP);//This computes the tf-time return time
 void stability(double **A);//This computes eigenvalues and eigenvectors to check stability
+void simulate_po(double *x,double tf);
 
 int main(int argc, char * argv[]){
-  int i;
+  int i,result;
   double *x=new double[n];
   double **DP=new double*[n];
   for (i=0;i<n;i++){
@@ -42,16 +43,20 @@ int main(int argc, char * argv[]){
     sscanf(argv[i+1], "%lg", &x[i]);
   }
 
-  Newton_method(1,x,DP);
-
-  
-  cout <<"Periodic point:"<<endl;
-  for (i=0;i<n;i++){
-    cout<<x[i]<<" ";
+  result=Newton_method(1,x,DP);
+  if (result==1){
+    cout <<"Newton failed"<<endl;
   }
-  cout <<endl;
-  cout <<"Eigenvalues and eigenvectors of Ds"<<endl;
-  stability(DP);
+  else{
+    simulate_po(x,T);
+    cout <<"Periodic point:"<<endl;
+    for (i=0;i<n;i++){
+      cout<<x[i]<<" ";
+    }
+    cout <<endl;
+    cout <<"Eigenvalues and eigenvectors of Ds"<<endl;
+    stability(DP);
+  }
 
   delete[] x;
   for (i=0;i<n;i++){
@@ -93,7 +98,6 @@ int Newton_method(int q,double *xout,double **DPout){
   double *p;
   double *dta;
   int maxiterNewton=500;
-  int res=0;
   double *x=new double[n];
   double **DP;
   DP=crear_matriu(0,n-1,0,n-1);
@@ -109,20 +113,11 @@ int Newton_method(int q,double *xout,double **DPout){
   tf=double(q)*T;
 
   for (i=0;i<n;i++){
-  xa[i]=x[i];
+    xa[i]=x[i];
   }  
 /*integrate*/
   
-  res=strobomap(x, tf, DP);
-  if (res==1){
-    free(xa);
-    free(perm);
-    free(dta);
-    free(p);
-    alliberar_matriu(DP,0,n-1,0);
-    delete[] x;
-    return 1;
-  }
+  strobomap(x, tf, DP);
   dist=0;
   for (i=0;i<n;i++){
     dist+=(x[i]-xa[i])*(x[i]-xa[i]);
@@ -163,22 +158,26 @@ int Newton_method(int q,double *xout,double **DPout){
         x[i]=xa[i];
      }
 
-     res=strobomap(x, tf, DP);
-    if (res==1){
-      free(xa);
-      free(perm);
-      free(dta);
-      free(p);
-      alliberar_matriu(DP,0,n-1,0);
-      delete[] x;
-      return 1;
-    }
+     strobomap(x, tf, DP);
+     //For debuggin purposes
+     /*
+     int j;
+     for (i=0;i<n;i++){
+       cout <<0<<" ";
+       for (j=0;j<n;j++){
+	 cout <<DP[i][j]<<" ";
+       }
+       cout <<endl;
+     }
+     */
 
      dist=0;
      for (i=0;i<n;i++){
        dist+=(x[i]-xa[i])*(x[i]-xa[i]);
      }
      dist=sqrt(dist);
+     //For debuggin purposes
+     //cout <<"-2 "<<dist<<endl;
 
      nite++;
   }
@@ -188,13 +187,13 @@ int Newton_method(int q,double *xout,double **DPout){
   free(p);
 
    if (nite==maxiterNewton && dist > NewtonTol || isnan(x[0]) ||isnan(x[1])){
-     //printf("#massa iteracions newton\n");
-     //exit(1);
      alliberar_matriu(DP,0,n-1,0);
      delete[] x;
      return 1;
    }
    else {
+     cout<<"Newton iterations: "<<nite<<endl;
+     cout <<"Error: "<<dist<<endl;
      for (i=0;i<n;i++){
        xout[i]=x[i];
        for (j=0;j<n;j++){
@@ -213,10 +212,10 @@ int strobomap(double* xini, double tf, double **DP){
   int ndim=n+n*n;//Dimension of the system with the variational equations
   double *dx=new double[ndim];
   double *x=new double[ndim];
-  double hini=0.05;
-  double h=hini;
-  double hmin=1.0E-4;
-  double hmax=1.0;
+  double h=0.05;
+  double hmin=1.0E-6;
+  double hmax=1e-2;
+  //double hmax=1e-1;
   double tol=1.0e-13;
 
   for (i=0;i<n;i++){
@@ -233,9 +232,26 @@ int strobomap(double* xini, double tf, double **DP){
     }
   }
  ini_rk78(ndim);
+ //For debugging purposes:
+ /*
+ cout<<-1<<" "<<t<<" ";
+ for (i=0;i<ndim;i++){
+   cout<<x[i]<<" ";
+ }
+ cout <<endl;
+ */
  while (t<t0+tf){
   rk78(&t,x,&h,tol,hmin,hmax,ndim,vfield);
+   //For debugging purposes:
+   /*
+  cout<<-1 <<" "<<t<<" ";
+   for (i=0;i<ndim;i++){
+     cout<<x[i]<<" ";
+   }
+ cout <<endl;
+ */
  }
+ //cout<<endl;
 
  h=-(t-tf-t0);
  rk78(&t,x,&h,tol,fabs(h),fabs(h),ndim,vfield);
@@ -244,7 +260,6 @@ int strobomap(double* xini, double tf, double **DP){
  for (i=0;i<n;i++){
    xini[i]=x[i];
  }
-
  for (i=0;i<n;i++){
    for (j=0;j<n;j++){
      DP[i][j]=x[(i+1)*n+j];
@@ -254,19 +269,6 @@ int strobomap(double* xini, double tf, double **DP){
  delete[] dx;
  delete[] x;
  return 0;
-}
-
-void vfield(double t, double *x, int ndim, double *dx){
-  //Equations of the pendulum:
-  double eps=5e-2;
-  dx[0]=x[1];
-  dx[1]=-sin(x[0])+eps*sin(twopi/T*t);
-
-  //Variational equations:
-  dx[2]=x[4];
-  dx[3]=x[5];
-  dx[4]=-cos(x[0])*x[2];
-  dx[5]=-cos(x[0])*x[3];
 }
 
 void stability(double **A){
@@ -315,4 +317,98 @@ void stability(double **A){
       }
   }
 }
+/*
+void vfield(double t, double *x, int ndim, double *dx){
+  //Equations of the pendulum:
+  double eps=5e-2;
+  dx[0]=x[1];
+  dx[1]=-sin(x[0])+eps*sin(twopi/T*t);
 
+  //Variational equations:
+  dx[2]=x[4];
+  dx[3]=x[5];
+  dx[4]=-cos(x[0])*x[2];
+  dx[5]=-cos(x[0])*x[3];
+}
+*/
+
+/*
+void vfield(double t, double *x, int ndim, double *dx){
+  //Landau oscillator
+  dx[0]=x[0]-x[1]-x[0]*(pow(x[0],2)+pow(x[1],2));
+  dx[1]=x[0]+x[1]-x[1]*(pow(x[0],2)+pow(x[1],2))+sin(t);
+  //Variational equations
+  double J1=1 -3.*pow(x[0],2.)+pow(x[1],2.);
+  double J2=-1-2.*x[0]*x[1];
+  double J3=1-2.*x[1]*x[0];
+  double J4=1-pow(x[0],2.)+3.*pow(x[1],2.);
+  dx[2]=J1*x[2]+J2*x[4];
+  dx[3]=J1*x[3]+J2*x[5];
+  dx[4]=J3*x[2]+J4*x[4];
+  dx[5]=J3*x[3]*J4*x[5];
+}
+  */
+
+void vfield(double t, double *x, int ndim, double *dx){
+  //Forced hyperbolic point
+  double omega=twopi/T;
+  dx[0]=x[1];
+  dx[1]=x[0]+0.5*sin(omega*t);
+
+  //Variational equations:
+  double J1=0;
+  double J2=1;
+  double J3=1;
+  double J4=0;
+  dx[2]=J1*x[2]+J2*x[4];
+  dx[3]=J1*x[3]+J2*x[5];
+  dx[4]=J3*x[2]+J4*x[4];
+  dx[5]=J3*x[3]+J4*x[5];
+}
+
+void simulate_po(double *x,double tf){
+  double t=t0;
+  int n=2;
+  int ndim=n+n*n;
+  double twopi=8*atan(1.);
+  double h=0.05;
+  double hmin=1.0E-4;
+  double hmax=1e-2;
+  double tol=1.0e-13;
+  double *xv=new double[ndim];
+  int i;
+
+  ofstream ofile;
+  ofile.precision(10);
+  ofile.open("trajectory.dat");
+  for (i=n;i<ndim;i++){
+    if ((i-n)%(n+1)==0){
+      xv[i]=1.;
+    }
+    else{
+      xv[i]=0.;
+    }
+  }
+  ini_rk78(ndim);
+  while (t<t0+tf){
+    ofile<<" "<<t<<" ";
+    for (i=0;i<ndim;i++){
+     ofile<<xv[i]<<" ";
+    }
+    ofile <<endl;
+    rk78(&t,xv,&h,tol,hmin,hmax,ndim,vfield);
+  }
+  //cout<<endl;
+
+  h=-(t-tf-t0);
+  rk78(&t,xv,&h,tol,fabs(h),fabs(h),ndim,vfield);
+  ofile<<" "<<t<<" ";
+  for (i=0;i<ndim;i++){
+   ofile<<xv[i]<<" ";
+  }
+  ofile <<endl;
+  end_rk78(ndim);
+  ofile.close();
+
+  delete [] xv;
+}
